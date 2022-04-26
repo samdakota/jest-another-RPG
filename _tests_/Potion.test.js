e12686f74778baebf8e215ea4b1a3f0894ad5777
Sam Miller <sam.dakota@gmail.com>
2022-04-26 13:17:02 -0400

    Player constructor

--- _tests_/Potion.test.js
+++ _tests_/Potion.test.js
@@ -1,5 +1,11 @@
-const { exportAllDeclaration } = require('@babel/types');
 const Potion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+  
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
 
 test ('creates a random potion object', () => {
     const potion = new Potion();
